1eefdfe471ae31dec7cfa3fa7c9af48f8084369d
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-11-23 20:15:35 +0300

    Bottom Navigation menu update

--- app/src/main/java/com/learning/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/learning/newsapp/ui/NewsActivity.kt
@@ -2,6 +2,8 @@ package com.learning.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.learning.newsapp.R
 import com.learning.newsapp.databinding.ActivityNewsBinding
 import kotlinx.android.synthetic.main.activity_news.*
@@ -10,10 +12,11 @@ class NewsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
         var view = binding.root
 //        setContentView(R.layout.activity_news)
-        setContentView(view)
+        setContentView(binding.root)
 
-        bottomNavigationView
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
