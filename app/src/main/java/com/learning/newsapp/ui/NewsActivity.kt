7541c0e1564b6f1469df992b77c61ca236adb00b
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-12-11 01:07:59 +0300

    Architecture side

--- app/src/main/java/com/learning/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/learning/newsapp/ui/NewsActivity.kt
@@ -2,20 +2,29 @@ package com.learning.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.learning.newsapp.R
 import com.learning.newsapp.databinding.ActivityNewsBinding
+import db.ArticleDatabase
 import kotlinx.android.synthetic.main.activity_news.*
+import repository.NewsRepository
 
 class NewsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewsBinding
+    lateinit var viewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsBinding.inflate(layoutInflater)
         var view = binding.root
 //        setContentView(R.layout.activity_news)
         setContentView(binding.root)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
 
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
